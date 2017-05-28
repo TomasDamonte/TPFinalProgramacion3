efccd7ee2093827618f96005fd595e30ea4a63f9
Tomas <tomas.damonte@comunidad.ub.edu.ar>
2017-05-28 10:41:02 -0300

    first commit

---
+++ src/algoritmo/genetico/Program.java
@@ -0,0 +1,12 @@
+package algoritmo.genetico;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("unused")
+		Controlador controlador = new Controlador();
+		
+	}
+
+}
