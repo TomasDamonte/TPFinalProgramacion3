853ae5310cc2f957dbcc195a13c08e49c86f4f27
Tomas <tomas.damonte@comunidad.ub.edu.ar>
2017-05-30 10:54:00 -0300

    -Error de tipeo

--- ProyectoAlgoritmoGenetico/src/ar/edu/ub/proyectoalgoritmogenetico/controlador/AlgoritmoGeneticoControlador.java
+++ ProyectoAlgoritmoGenetico/src/ar/edu/ub/proyectoalgoritmogenetico/controlador/AlgoritmoGeneticoControlador.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
-import ar.edu.ub.proyectoalgorimogenetico.vista.Vista;
+import ar.edu.ub.proyectoalgoritmogenetico.vista.Vista;
 import ar.edu.ub.proyectoalgoritmogenetico.modelo.AlgoritmoGenetico;
 
 public class AlgoritmoGeneticoControlador implements ActionListener {
