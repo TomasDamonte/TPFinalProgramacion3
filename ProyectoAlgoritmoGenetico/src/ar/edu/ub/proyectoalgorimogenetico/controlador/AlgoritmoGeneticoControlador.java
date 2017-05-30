604be41866791bb054566037917de014fbd42515
Tomas <tomas.damonte@comunidad.ub.edu.ar>
2017-05-30 10:24:18 -0300

    -Renombrado de Algoritmo genetico a ProyectoAlgoritmoGenetico

---
+++ ProyectoAlgoritmoGenetico/src/ar/edu/ub/proyectoalgorimogenetico/controlador/AlgoritmoGeneticoControlador.java
@@ -0,0 +1,32 @@
+package ar.edu.ub.proyectoalgorimogenetico.controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+import ar.edu.ub.proyectoalgorimogenetico.vista.Vista;
+import ar.edu.ub.proyectoalgoritmogenetico.modelo.AlgoritmoGenetico;
+
+public class AlgoritmoGeneticoControlador implements ActionListener {
+	private Vista vista;
+	AlgoritmoGenetico algen = new AlgoritmoGenetico();
+	
+	public AlgoritmoGeneticoControlador() {
+		this.vista = new Vista(this);
+		vista.getClave().setText(algen.getClave().getHexadecimalString() + "\n" + algen.getClave().getBinarioString());
+		vista.mostrar();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == vista.getBtnBuscar()) {
+			try {
+				algen.buscar(algen.getClave());
+				vista.getLblNumeroIteraciones().setText(algen.getCantidadGeneraciones().toString());
+				vista.getListaPoblacion().setText(algen.getPoblacion().getPoblacionString());
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}		
+	}
+	
+}
